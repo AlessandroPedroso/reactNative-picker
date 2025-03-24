0e250b28aa9e3e4cb41e5b321b0d4aeb2af00fa6
Your Name <alessanrospedroso@outlook.com>
2025-03-24 12:49:20 -0300

    sobre picker finalizado

--- App.js
+++ App.js
@@ -1,11 +1,59 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { Picker } from "@react-native-picker/picker";
+import { useState } from "react";
 
 export default function App() {
+  const [carroSelecionado, setCarroSelecionado] = useState(0);
+  const [carros, setCarros] = useState([
+    {
+      key: 1,
+      nome: "Golf 1.6",
+      valor: 62.555,
+    },
+    {
+      key: 2,
+      nome: "Saveiro 1.6",
+      valor: 29.325,
+    },
+    {
+      key: 3,
+      nome: "Gol 1.0",
+      valor: 25.222,
+    },
+    {
+      key: 4,
+      nome: "BMW 120I",
+      valor: 125.222,
+    },
+    {
+      key: 5,
+      nome: "Uno 1.0",
+      valor: 25.222,
+    },
+  ]);
+
+  let carrosItem = carros.map((value, key) => {
+    return <Picker.Item key={key} value={key.toString()} label={value.nome} />;
+  });
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Picker
+        style={styles.stylePicker}
+        selectedValue={carroSelecionado}
+        onValueChange={(itemValue, itemIndex) => setCarroSelecionado(itemValue)}
+      >
+        {carrosItem}
+        {/* <Picker.Item key={0} value="0" label="Golf 1.6" />
+        <Picker.Item key={1} value="1" label="Saveiro 1.6" />
+        <Picker.Item key={2} value="2" label="Gol 1.0" /> */}
+      </Picker>
+
+      <Text style={styles.carros}>Carro: {carros[carroSelecionado].nome}</Text>
+      <Text style={styles.carros}>
+        R$: {carros[carroSelecionado].valor.toFixed(2)}
+      </Text>
     </View>
   );
 }
@@ -13,8 +61,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 135,
+  },
+  stylePicker: {
+    width: "auto",
+  },
+  carros: {
+    marginTop: 15,
+    fontSize: 25,
   },
 });
